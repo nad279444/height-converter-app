5d2bb76d861fd8141c7f84163c4442b2a1254b8d
bernard frimpong <nardfrimpong@gmail.com>
2020-05-28 18:03:37 +0000

    height converter

---
+++ index.js
@@ -0,0 +1,9 @@
+const height = document.forms["box"]
+height.addEventListener("submit",function(e){
+    e.preventDefault();
+    const feet = height.querySelector('input[type="number"]').value * 30.48
+    const inches = height.querySelector('input[type="number"]:nth-child(2)#inches').value * 2.54
+    document.getElementById('output').innerHTML=  Math.floor(feet + inches)
+
+})
+
