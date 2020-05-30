c70077edac443a10fa2988f99ed25c795acd9327
bernard frimpong <nardfrimpong@gmail.com>
2020-05-30 07:30:14 +0000

    added cm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ height.addEventListener("submit",function(e){
     e.preventDefault();
     const feet = height.querySelector('input[type="number"]').value * 30.48
     const inches = height.querySelector('input[type="number"]:nth-child(2)#inches').value * 2.54
-    document.getElementById('output').innerHTML=  Math.floor(feet + inches)
+    document.getElementById('output').innerHTML=  Math.floor(feet + inches) + 'cm'
 
 })
 
